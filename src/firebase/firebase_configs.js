51973fbdad328d220f6523dd7f8550205f8e207b
aghermanHP <adrian.gherman1@gmail.com>
2020-03-09 11:13:15 +0200

    was aded configs to project

---
+++ src/firebase/firebase_configs.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCkebzRKwLPmNow8rBi2CRaehYzvLCYhnw",
+    authDomain: "qa-labs-35bb1.firebaseapp.com",
+    databaseURL: "https://qa-labs-35bb1.firebaseio.com",
+    projectId: "qa-labs-35bb1",
+    storageBucket: "qa-labs-35bb1.appspot.com",
+    messagingSenderId: "576902743921",
+    appId: "1:576902743921:web:08f10587348448f11898d0",
+    measurementId: "G-2BEQ52WXS7"
+  };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as firebase from 'firebase'
+import { config } from './firebase/firebase_configs'
 
+firebase.initializeApp(config);
+firebase.analytics();
+Vue.prototype.$firebase = firebase;
 Vue.config.productionTip = false
 
 new Vue({
